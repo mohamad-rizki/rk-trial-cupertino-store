b1de0c7012c63ef6ba037943c379138fdebd6445
Mohamad Rizki Dafianto <mohamad.rizki.dafianto@gmail.com>
2021-02-10 23:44:57 +0700

    create the initial cupertino app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:rk_trial_cupertino_store/main.dart';
+import 'package:rk_trial_cupertino_store/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(CupertinoStoreApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
